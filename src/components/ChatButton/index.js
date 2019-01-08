aa4a28aae119521cbf7bed8fb65ec977951036c3
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-01-08 20:21:16 -0200

    Add ChatButton

---
+++ src/components/ChatButton/index.js
@@ -0,0 +1,15 @@
+import { h } from 'preact';
+import { createClassName } from '../helpers';
+import styles from './styles';
+import ChatIcon from '../../icons/plus.svg';
+import CloseIcon from '../../icons/close.svg';
+
+
+export const ChatButton = ({ open = false, ...args }) => (
+	<button {...args} type="button" className={createClassName(styles, 'chat-button', { open })}>
+		{open ? <CloseIcon /> : <ChatIcon />}
+	</button>
+);
+
+
+export default ChatButton;
---
+++ src/components/ChatButton/stories.js
@@ -0,0 +1,24 @@
+import { h } from 'preact';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
+import centered from '@storybook/addon-centered';
+import ChatButton from '.';
+
+
+storiesOf('Components|ChatButton', module)
+	.addDecorator(centered)
+	.addDecorator(withKnobs)
+	.add('normal', () => (
+		<ChatButton
+			open={boolean('open', false)}
+			onClick={action('clicked')}
+		/>
+	))
+	.add('opened', () => (
+		<ChatButton
+			open={boolean('open', true)}
+			onClick={action('clicked')}
+		/>
+	))
+;
